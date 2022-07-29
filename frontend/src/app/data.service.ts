73b5618ad95ed3ba187c1b07b2634423f1990a18
adam-street <adam.street.dev@gmail.com>
2022-07-29 14:57:51 -0500

    added frontend - backend connection

---
+++ frontend/src/app/data.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  // currentUser: any;
+  //   // currentUser$: Subject<any> = new Subject<any>();
+
+  // constructor(private httpService: HttpService) {
+  // }
+
+  // login(username: string, password: string) {
+  //   this.httpService.login(username, password).subscribe(
+  //     (data: ILoginResponse) => {
+  //       this.currentUser = data;
+  //       this.currentUser$.next(this.currentUser)
+  //     },
+  //     (error) => {
+  //       console.error(error);
+  //     }
+  //   )
+  // }
+}
